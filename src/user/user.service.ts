175a76c189742462a4fb90b694b31db19a73e623
Phelipe Matos (PHMATOS07) <phelipe.matos@viavarejo.com.br>
2023-09-26 18:14:27 -0300

    feat: Pré-criação do módulo de usuário, será o responsável pelo CRUD do mesmo.
    Author: Phelipe Matos (PHMATOS07)

---
+++ src/user/user.service.ts
@@ -0,0 +1,26 @@
+import { Error0001, Error0002 } from "src/models/error-object.const";
+import { ErrorObject } from "../models/error-object";
+import { UserDTO } from "../models/user.dto";
+
+export class UserService {
+
+  private users = new Array<UserDTO>();
+
+  create(user: UserDTO): ErrorObject {
+    this.users.push(user);
+    return Error0001;
+  }
+
+  consult(email: string): UserDTO | ErrorObject {
+    const user = this.users.find(user => user.email === email);
+
+    if (user) {
+      return user;
+    }
+    return Error0002;
+  }
+
+  consultAll(): UserDTO[] {
+    return this.users;
+  }
+}
